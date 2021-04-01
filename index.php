667432b461021b0b2db697e0b7b69ba42b3cfc85
itsmedeepmca <itsmedeepmca@gmail.com>
2021-04-01 18:50:03 +0530

    Login & Reg page completed

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+   <div class="container">
+       <div class="row">
+           <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+             <table class="table">
+                 <h1>Login</h1>
+               <tr>
+                <td>Username</td>
+                <td> <input type="text" class="form-control"></td>
+              </tr>
+            <tr>
+                <td>Password </td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button class="btn btn-success">Login</button></td>
+            </tr>
+            </table>
+           </div>
+           <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+               <table class="table">
+                   <h1>Registrattion</h1>
+                   <tr>
+                       <td>Name </td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td>Address</td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td> Place</td>
+                       <td> <input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td> Email Id </td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td> Phone Number</td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td> Username </td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td> Password </td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td>Confirm password </td>
+                       <td> <input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td></td>
+                       <td><Button class="btn btn-info">Register</Button></td>
+                   </tr>
+               </table>
+           </div>
+       </div>
+   </div> 
+</body>
+</html>
